dbc18148f56f264288550f2949f88e07f49ec14a
byemane <beteyemane@hotmail.co.uk>
2019-03-26 19:49:56 +0000

    basic function on getting weather appropriate event

--- src/components/EventsTown.js
+++ src/components/EventsTown.js
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import EventCard from './EventCard'
 
+let eventType = ''
 
 class EventsTown extends React.Component {
   constructor() {
@@ -9,13 +10,21 @@ class EventsTown extends React.Component {
 
     this.state = {
       events: [],
-      weather: []
+      weather: [],
+      eventWeather: []
     }
-    this.weatherEvents.bind = this.weatherEvents.bind(this)
+    // this.weatherEvents.bind = this.weatherEvents.bind(this)
   }
 
 
   componentDidMount() {
+
+    const { temp_c } = this.props.location.state.weather
+    if(temp_c >= '20') {
+      eventType = 'FEST'
+    } else {
+      eventType = 'LIVE'
+    }
 
     const { lat, lng } = this.props.location.state.latLng
     axios.get('https://cors-anywhere.herokuapp.com/https://www.skiddle.com/api/v1/events/', {
@@ -23,18 +32,20 @@ class EventsTown extends React.Component {
         api_key: '0c64ae5cca7903c86353520198c58021',
         latitude: lat,
         longitude: lng,
-        radius: 5
+        radius: 5,
+        eventcode: eventType
       }
     })
       .then(response => this.setState({events: response.data.results}))
   }
 
-  weatherEvents() {
-    this.state.events.forEach(event => {
-      if(event.EventCode === 'LIVE' )
-        console.log('LIVE')
-    })
-  }
+
+  // weatherEvents() {
+  //   const { temp_c } = this.props.location.state.weather
+  //   if(temp_c >= '20') {
+  //     eventType = 'FEST'
+  //   }
+  // }
 
   render() {
     console.log(this.state.events)
@@ -44,7 +55,6 @@ class EventsTown extends React.Component {
     return (
       <main className="section">
         <div className="container">
-          {this.weatherEvents()}
           <h1>{temp_c}</h1>
           <h1>{date}</h1>
           <div className="columns is-multiline">
